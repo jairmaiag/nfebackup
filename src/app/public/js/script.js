b841fe9db154f9abcabc3db258fa655f6e8f07c4
Jair Maia Diniz <jairmaiag@gmail.com>
2022-09-03 17:35:20 -0300

    Barra lateral

--- src/app/public/js/script.js
+++ src/app/public/js/script.js
@@ -1,26 +1,27 @@
-const form = document.getElementById("fCostumer");
-function criarCampoJson(objInput) {
-    const jsonString = `"${objInput.name}":"${objInput.value}"`;
-    return jsonString;
-}
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const campos = form.querySelectorAll('input');
-    let strEnvio = '';
-    campos.forEach(campo => {
-        if (campo.value) {
-            strEnvio += criarCampoJson(campo);
-            strEnvio += ',';
-        }
-    });
-    if (strEnvio.length) {
-        strEnvio = strEnvio.substring(0, strEnvio.length - 1);
-        strEnvio = '{' + strEnvio + '}';
-    }
-    const retorno = $.post("/nfeCustomer", JSON.parse(strEnvio), "json");
-    retorno.done(function () {
-        M.toast({ html: 'Cadastro realizado com sucesso!', classes: 'rounded' });
-    }).fail(function (erro) {
-        $("#alerta").empty().append(erro.responseText);
-    });
+$(document).ready(function () {
+    $('.sidenav').sidenav();
 });
+const form = document.getElementById("fCostumer");
+if (form) {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const campos = form.querySelectorAll('input');
+        let strEnvio = '';
+        campos.forEach(campo => {
+            if (campo.value) {
+                strEnvio += `"${objInput.name}":"${objInput.value}"`;
+                strEnvio += ',';
+            }
+        });
+        if (strEnvio.length) {
+            strEnvio = strEnvio.substring(0, strEnvio.length - 1);
+            strEnvio = '{' + strEnvio + '}';
+        }
+        const retorno = $.post("/nfeCustomer", JSON.parse(strEnvio), "json");
+        retorno.done(function () {
+            M.toast({ html: 'Cadastro realizado com sucesso!', classes: 'rounded' });
+        }).fail(function (erro) {
+            $("#alerta").empty().append(erro.responseText);
+        });
+    });
+}
