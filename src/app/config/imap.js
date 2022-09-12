cb0e4fd6c5a8124badbdcaea8b35a49efc5a3be0
Jair Maia Diniz <jairmaiag@gmail.com>
2022-09-12 15:58:05 -0300

    100 - Mover emails para a lixeira

--- src/app/config/imap.js
+++ src/app/config/imap.js
@@ -20,18 +20,26 @@ const imap = new Imap({
 });
 
 imap.on("ready", function () {
-  imap.openBox("INBOX", true, function (err, box) {
+  imap.openBox("INBOX", false, function (err, box) {
     if (err) throw err;
+
     const defaultDate = process.env.DEFAULT_DATE;
     const searchDate = formatMesDiaAno(
       new Date(imap._config.searchDate || defaultDate)
     );
     imap.qtdNfe = 0;
     imap.qtdEmail = 0;
+
     imap.search(
       [["OR", "UNSEEN", ["SINCE", searchDate]]],
+
       function (err, results) {
         if (err) throw err;
+
+        if (results.length === 0) {
+          imap.end();
+          return;
+        }
 
         imap.qtdEmail += results.length;
         const imapFetch = imap.fetch(results, {
@@ -39,19 +47,20 @@ imap.on("ready", function () {
           struct: true,
         });
 
-        imapFetch.on("message", function (msg, seqno) {
-          msg.on("attributes", function (attrs) {
+        imapFetch.on("message", function (emailMessage, seqno) {
+          emailMessage.on("attributes", function (attrs) {
             const attachments = findAttachmentParts(attrs.struct);
             imap.qtdNfe += attachments.length;
-            attachments.forEach((attachment) => {
-              if (isXml(attachment)) {
+            attachments
+              .filter(attac => isXml(attac))
+              .forEach((attachment) => {
+                imap.addFlags(seqno, '\\Deleted');
                 const fetch = imap.fetch(attrs.uid, {
                   bodies: [attachment.partID],
                   struct: true,
                 });
                 fetch.on("message", buildAttMessageFunction(attachment));
-              }
-            });
+              });
           });
         });
 
